06e509843efb82c32e15a4e54cd6df94d8f99428
Soubhik <Soubhik@Soubhik-PC>
2017-11-06 16:20:39 +0000

    fg

--- checkProcess.php
+++ checkProcess.php
@@ -1,11 +1,29 @@
 <?php 
 
+if(isset($_REQUEST['pn'])){
+	checkProcesses($_REQUEST['pn']);
+}else{
+	checkProcesses("all");
+}
+function checkProcesses($processName){
 exec("tasklist 2>NUL", $task_list);
 
-foreach($task_list as $tasks){
-	echo $tasks."<br/>";
-}
+	// if all the processes needs to be returned
+
+	//if($processName==="all"){return $task_list;}
+	
+	// to check if the input process name is running in the server
+	
+	foreach ($task_list as $task){
+		//echo $tasks."<br/>";
+		if($task==$processName){
+			echo $processName." is running in the server";
+		}else{
+			echo $processName." is NOT running in the server";
+		}
+	}
+
 
 //print_r($task_list);
-
+}
 ?>
